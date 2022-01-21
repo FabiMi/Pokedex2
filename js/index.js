4b3488c5f9aee7cfec5292acb1cd1f899e674bc6
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-01-21 18:47:59 +0100

    added task1.3

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
+let pokemonList = [{
+    name:  "Bulbasaur",
+    height: 7,
+    type: ["grass, poison"]
+}, {name:  "chamander",
+    height: 8,
+    type: ["fire, stone"]},
+    {name:  "Squirtle",
+    height: 4,
+    type: ["water, stone"]}];
+
+let i = 0;
+
+for (;pokemonList[i];) { // create loop for pokemonList
+ let namee = pokemonList[i].name; // get Pokemon Name from pokemonList
+ let heightt = pokemonList[i].height; // *get Pokemon height from pokemonList
+ let typee = pokemonList[i].type; // get Pokemon type from pokemonList
+ i++; 
+
+ if (heightt > 7) {
+
+    document.write("name: " + namee + ", " + "height: (" + heightt + ")<-wow thats big!!!!, " + "type: " + typee + " "); // let loop appear with if in site
+   
+   } 
+else {
+    document.write("name: " + namee + ", " + "height: (" + heightt + "), " + "type: " + typee + " "); // let loop appear in site
+
+}
+
+ console.log (namee, heightt, typee); // call log for the loop
+}
+
+
+
+
+
+
+
+
+/*
+
 const pokedex = document.getElementById('pokedex');
 
 const fetchPokemon = () => {
@@ -13,8 +54,8 @@ const fetchPokemon = () => {
             type: result.types.map((type) => type.type.name).join(', '),
             id: result.id,
             height: result.height
+            
         }));
-        console.log(pokemon);
         displayPokemon(pokemon);
     });
 };
@@ -24,22 +65,31 @@ const displayPokemon = (pokemon) => {
     const pokemonHTMLString = pokemon
         .map(
             (pokeman) => `
-        <li class="card">
+        <li class="card" id="card">
+            
             <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
             <img class="card-image" src="${pokeman.image}"/>
             <p class="card-subtitle">Type: ${pokeman.type}</p>
-            <p class="card-subtitle">Height: ${pokeman.height} feet</p>
+            <p class="card-subtitle">Height: ${pokeman.height} feed</p>
         </li>
     `
-        )   
+        )
         .join('');
-            
-
-        
-        
     pokedex.innerHTML = pokemonHTMLString;
 };
 
-
 fetchPokemon();
 
+let  card = document.getElementById("card");
+
+const card = "fire"
+
+if ("fire") in card =  {
+
+    card.style.backroundColor(yellow);
+else
+
+card.style.backroundColor(brown);
+}
+
+*/
